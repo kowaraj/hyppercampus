6eab1fad509a1e9d52926a91d6a1ca3cb4284cf3
kowaraj <kowaraj4stuff@gmail.com>
2016-09-20 22:41:48 +0200

    added tests

---
+++ src/vexx/test/test_db_add_node.clj
@@ -0,0 +1,55 @@
+(ns vexx.test.test-db-add-node
+  (:require
+   [vexx.model.debug :as dbg]
+   [vexx.model.item :as item]
+   [vexx.model.data :as data]
+   [vexx.model.db :as db]   
+   [vexx.model.item :as item]
+))
+
+(def db (ref (into {} (item/make-new-item "root"))))
+;@db
+
+(def path-1 [:root])
+
+;; no nodes yet
+(= (db/get-db-node db path-1) {})
+;; add a node 
+(= (db/add-db-node db path-1 "1")
+   {:root {:nodes {:1 {:nodes {}, :path nil, :link-to {}, :link-from {}}}, :path nil, :link-to {}, :link-from {}}})
+;; one node now
+(= (db/get-db-node db path-1)
+   {:1 {:nodes {}, :path nil, :link-to {}, :link-from {}}})
+
+(def path-2 [:root :1])
+
+;; add another node 
+(= (db/add-db-node db path-2 "1-1")
+   {:root {:nodes {:1 {:nodes {:1-1 {:nodes {}, :path nil, :link-to {}, :link-from {}}}, :path nil, :link-to {}, :link-from {}}}, :path nil, :link-to {}, :link-from {}}})
+;; check it
+(= (db/get-db-node db path-2)
+   {:1-1 {:nodes {}, :path nil, :link-to {}, :link-from {}}})
+
+(def path-3 [:root :1 :1-1])
+
+;; add attributes
+(db/set-db-node-attr db path-3 :path "root-1-11")
+
+(def path-4 [:root :1 :1-1])
+
+(db/del-db-node db path-4 "path")
+;@db
+
+{:root {:nodes
+        {:1 {:nodes
+             {:1-1 {:nodes {:path "root-1-11"},
+                    :path nil,
+                    :link-to {},
+                    :link-from {}}},
+             
+             :path nil, :link-to {}, :link-from {}}}, :path nil, :link-to {}, :link-from {}}}
+
+
+
+
+
---
+++ src/vexx/test/test_db_get_node.clj
@@ -0,0 +1,31 @@
+(ns vexx.test.test-db-get-node
+  (:require
+   [vexx.model.debug :as dbg]
+   [vexx.model.item :as item]
+   [vexx.model.data :as data]
+   [vexx.model.db :as db]   
+   [vexx.model.item :as item]
+))
+
+(def db (ref {
+              :root {
+                     :nodes
+                     (reduce into
+                             [{}
+                              (item/make-new-item "1")
+                              (item/make-new-item "2")
+                              ])
+                     :info "no info available"
+                     }
+              }))
+
+;@db
+
+            
+(def path-1 [:root :1])
+(def path-2 [:root :2])
+
+(db/get-db-node db [:root])
+(db/get-db-node db [:root :1])
+(:nodes (:root @db))
+(db/add-db-node db path-1 "1-1")
