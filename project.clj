67d57ce58fd63b212f16d84d24a10891054990fa
kowaraj <kowaraj4stuff@gmail.com>
2016-10-09 22:01:25 +0200

    fixed selection level-down

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject vexx "0.2.4-SNAPSHOT"
+(defproject vexx "0.2.5-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
